48ee4df25b3dddd9835ac7474d87b5b8bee4aac6
Jason Chi <jasonchi@Jasons-Mac-mini.ht.home>
2023-03-04 22:06:49 -0500

    fixed test files

--- src/database.py
+++ src/database.py
@@ -1,58 +1,85 @@
-import json
-from typing import Dict, List, Union
+import os
+import tempfile
+import unittest
+from src.database import Database
 from src.account import Account
 
+class TestDatabase(unittest.TestCase):
 
-class Database:
-    def __init__(self, accounts_file: str, customers_file: str):
-        self.accounts_file = accounts_file
-        self.customers_file = customers_file
-        self.accounts = self.load_accounts()
-        self.customers = self.load_customers()
+    def setUp(self):
+        self.accounts_file = tempfile.NamedTemporaryFile().name
+        self.customers_file = tempfile.NamedTemporaryFile().name
+        self.database = Database(self.accounts_file, self.customers_file)
 
-    def load_accounts(self) -> Dict[str, Account]:
-        try:
-            with open(self.accounts_file, 'r') as f:
-                accounts_json = json.load(f)
-                accounts = {}
-                for acc_num, acc_data in accounts_json.items():
-                    if acc_data['account_type'] == 'savings':
-                        accounts[acc_num] = SavingsAccount(acc_num, acc_data['name'], acc_data['balance'])
-                    elif acc_data['account_type'] == 'current':
-                        accounts[acc_num] = CurrentAccount(acc_num, acc_data['name'], acc_data['balance'], acc_data['overdraft'])
-                return accounts
-        except FileNotFoundError:
-            return {}
+    def tearDown(self):
+        os.remove(self.accounts_file)
+        os.remove(self.customers_file)
 
-    def load_customers(self) -> Dict[str, Dict[str, Union[str, List[str]]]]:
-        try:
-            with open(self.customers_file, 'r') as f:
-                customers_json = json.load(f)
-                return customers_json
-        except FileNotFoundError:
-            return {}
+    def test_load_accounts(self):
+        # create test data
+        account1 = SavingsAccount('1', 'Alice', 1000)
+        account2 = CurrentAccount('2', 'Bob', 500, 200)
+        self.database.save_account(account1)
+        self.database.save_account(account2)
 
-    def save_account(self, account: Account) -> None:
-        self.accounts[account.acc_num] = account
-        self.save_accounts()
+        # reload accounts from database
+        self.database.accounts = self.database.load_accounts()
 
-    def save_accounts(self) -> None:
-        accounts_json = {}
-        for acc_num, account in self.accounts.items():
-            acc_data = {'account_type': account.__class__.__name__.lower(),
-                        'name': account.name,
-                        'balance': account.balance}
-            if isinstance(account, CurrentAccount):
-                acc_data['overdraft'] = account.overdraft
-            accounts_json[acc_num] = acc_data
-        with open(self.accounts_file, 'w') as f:
-            json.dump(accounts_json, f)
+        # check if accounts were loaded correctly
+        self.assertEqual(len(self.database.accounts), 2)
+        self.assertIsInstance(self.database.accounts['1'], SavingsAccount)
+        self.assertEqual(self.database.accounts['1'].name, 'Alice')
+        self.assertEqual(self.database.accounts['1'].balance, 1000)
+        self.assertIsInstance(self.database.accounts['2'], CurrentAccount)
+        self.assertEqual(self.database.accounts['2'].name, 'Bob')
+        self.assertEqual(self.database.accounts['2'].balance, 500)
+        self.assertEqual(self.database.accounts['2'].overdraft, 200)
 
-    def save_customer(self, customer_id: str, customer_data: Dict[str, Union[str, List[str]]]) -> None:
-        self.customers[customer_id] = customer_data
-        self.save_customers()
+    def test_save_account(self):
+        # create test data
+        account1 = SavingsAccount('1', 'Alice', 1000)
 
-    def save_customers(self) -> None:
-        with open(self.customers_file, 'w') as f:
-            json.dump(self.customers, f)
+        # save account to database
+        self.database.save_account(account1)
+
+        # reload accounts from database
+        self.database.accounts = self.database.load_accounts()
+
+        # check if account was saved correctly
+        self.assertEqual(len(self.database.accounts), 1)
+        self.assertIsInstance(self.database.accounts['1'], SavingsAccount)
+        self.assertEqual(self.database.accounts['1'].name, 'Alice')
+        self.assertEqual(self.database.accounts['1'].balance, 1000)
+
+    def test_load_customers(self):
+        # create test data
+        customer1 = {'name': 'Alice', 'accounts': ['1']}
+        customer2 = {'name': 'Bob', 'accounts': ['2']}
+        self.database.save_customer('1', customer1)
+        self.database.save_customer('2', customer2)
+
+        # reload customers from database
+        self.database.customers = self.database.load_customers()
+
+        # check if customers were loaded correctly
+        self.assertEqual(len(self.database.customers), 2)
+        self.assertEqual(self.database.customers['1']['name'], 'Alice')
+        self.assertEqual(self.database.customers['1']['accounts'], ['1'])
+        self.assertEqual(self.database.customers['2']['name'], 'Bob')
+        self.assertEqual(self.database.customers['2']['accounts'], ['2'])
+
+    def test_save_customer(self):
+        # create test data
+        customer1 = {'name': 'Alice', 'accounts': ['1']}
+
+        # save customer to database
+        self.database.save_customer('1', customer1)
+
+        # reload customers from database
+        self.database.customers = self.database.load_customers()
+
+        # check if customer was saved correctly
+        self.assertEqual(len(self.database.customers), 1)
+        self.assertEqual(self.database.customers['1']['name'], 'Alice')
+        self.assertEqual(self.database.customers['1']['accounts'], ['1'])
 
--- tests/test_customer.py
+++ tests/test_customer.py
@@ -1,31 +1,42 @@
 import unittest
 from src.customer import Customer
 
-
 class TestCustomer(unittest.TestCase):
 
     def test_customer_creation(self):
-        customer = Customer('John', 'Doe', 'johndoe@example.com', 'password')
-        self.assertEqual(customer.first_name, 'John')
-        self.assertEqual(customer.last_name, 'Doe')
-        self.assertEqual(customer.email, 'johndoe@example.com')
-        self.assertEqual(customer.password, 'password')
+        customer = Customer("John Doe", "johndoe@example.com", "password", "123")
+        self.assertEqual(customer.name, "John Doe")
+        self.assertEqual(customer.email, "johndoe@example.com")
+        self.assertEqual(customer.password, "password")
+        self.assertEqual(customer.account_number, "123")
+        self.assertEqual(customer.balance, 0)
 
-    def test_customer_deposit(self):
-        customer = Customer('John', 'Doe', 'johndoe@example.com', 'password')
+    def test_deposit(self):
+        customer = Customer("John Doe", "johndoe@example.com", "password", "123")
         customer.deposit(100)
         self.assertEqual(customer.balance, 100)
 
-    def test_customer_withdraw(self):
-        customer = Customer('John', 'Doe', 'johndoe@example.com', 'password')
+    def test_withdraw(self):
+        customer = Customer("John Doe", "johndoe@example.com", "password", "123")
         customer.deposit(100)
         customer.withdraw(50)
         self.assertEqual(customer.balance, 50)
 
-    def test_customer_insufficient_funds(self):
-        customer = Customer('John', 'Doe', 'johndoe@example.com', 'password')
-        with self.assertRaises(ValueError):
-            customer.withdraw(50)
+    def test_insufficient_balance(self):
+        customer = Customer("John Doe", "johndoe@example.com", "password", "123")
+        customer.deposit(100)
+        customer.withdraw(150)
+        self.assertEqual(customer.balance, 100)
+
+    def test_view_balance(self):
+        customer = Customer("John Doe", "johndoe@example.com", "password", "123")
+        customer.deposit(100)
+        self.assertEqual(customer.view_balance(), "Your balance is 100")
+
+    def test_view_details(self):
+        customer = Customer("John Doe", "johndoe@example.com", "password", "123")
+        details = "Name: John Doe\nEmail: johndoe@example.com\nAccount Number: 123\nBalance: 0"
+        self.assertEqual(customer.view_details(), details)
 
 if __name__ == '__main__':
     unittest.main()
